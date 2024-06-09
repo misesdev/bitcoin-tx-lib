41b25f38cb8f25f220076ad5ca4483c78e3b9cbe
Mises Dev <misesdev@onionmail.org>
2024-06-09 03:56:25 -0300

    firs publication

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+
+export function generateKeyPair() {
+
+    console.log(`generate a new key pair public and private bitcoin keys`)
+}
+
+export function buildTransaction(tx: Transaction) {
+
+    console.log(`return transaction in the version ${tx.version}}`)
+}
+
