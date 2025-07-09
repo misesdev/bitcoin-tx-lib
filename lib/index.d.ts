42bb88b3bb827510509ece65b47de800f523cd06
Mises Dev <misesdev@onionmail.org>
2025-07-09 16:49:59 -0300

    onchain security testing

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,7 @@
 export * from "./src/types";
 export * from "./src/ecpairkey";
 export * from "./src/transaction";
+export * from "./src/hdkmanager";
 export * from "./src/hdtransaction";
 export * from "./src/hdwallet";
 export * from "./src/utils/address";
